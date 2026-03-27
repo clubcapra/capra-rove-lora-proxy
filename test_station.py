e50b59506803072438667e71583eb06a752c871a
mapleduck <yanhaddar@gmail.com>
2026-03-27 15:21:30 -0400

    reorganizing

--- test_station.py
+++ test_station.py
@@ -1,7 +1,7 @@
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(('0.0.0.0', 507))  # bind to local port FIRST
-s.sendto(b'https://pastebin.com/FDYEbETf', ('192.168.2.13', 505))
+s.sendto(b'Message sent from Station', ('192.168.2.13', 505))
 print('sent')
 
 # now also listen for the reply
